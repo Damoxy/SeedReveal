6a02b1de544fbf208d578edee65e7a10914345c1
Damola Obadeji <40131855+Damoxy@users.noreply.github.com>
2025-09-17 18:56:41 +0100

    Update seeds.py

--- utils/seeds.py
+++ utils/seeds.py
@@ -1,6 +1,7 @@
 import random
 
 def get_shuffled_seeds():
-    seeds = [f"{chr(i)}{j}" for i in range(ord('A'), ord('H') + 1) for j in (1, 2)]
+    seeds = [f"{chr(i)}2" for i in range(ord('A'), ord('P') + 1)]
     random.shuffle(seeds)
     return seeds
+
